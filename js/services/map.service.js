7f814e1be3c1ce0efadacfec0fbc90a23301ed9d
InonBer <inon551@gmail.com>
2022-06-22 14:29:42 +0300

    first commit

---
+++ js/services/map.service.js
@@ -0,0 +1,62 @@
+
+
+export const mapService = {
+    initMap,
+    addMarker,
+    panTo
+}
+
+var gMap;
+// gMap.addListener(gMap, "click", function (event) {
+//     var place = addMarker(event.latLng, map);
+//     console.log(place);
+// });
+
+function initMap(lat = 32.0749831, lng = 34.9120554) {
+    console.log('InitMap');
+    return _connectGoogleApi()
+        .then(() => {
+            console.log('google available');
+            gMap = new google.maps.Map(
+                document.querySelector('#map'), {
+                center: { lat, lng },
+                zoom: 15
+            })
+            gMap.addListener('click', function (event) {
+                addMarker(event.latLng)
+                console.log(event);
+            })
+            console.log('Map!', gMap);
+        })
+}
+
+function addMarker(loc) {
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: gMap,
+        title: 'Hello World!'
+    });
+    return marker;
+}
+
+function panTo(lat, lng) {
+    var laLatLng = new google.maps.LatLng(lat, lng);
+    gMap.panTo(laLatLng);
+}
+
+
+
+function _connectGoogleApi() {
+    if (window.google) return Promise.resolve()
+    const API_KEY = 'AIzaSyBMAM9h9PFldbQkiUoOaRXJu5I9EH5MeMQ'; //TODO: Enter your API Key
+    var elGoogleApi = document.createElement('script');
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    elGoogleApi.async = true;
+    document.body.append(elGoogleApi);
+
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve;
+        elGoogleApi.onerror = () => reject('Google script failed to load')
+    })
+}
+
